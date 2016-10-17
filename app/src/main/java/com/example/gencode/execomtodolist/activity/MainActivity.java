99378dc1d48e93a2046e04a70daddeae2b97b635
for3v3rInFriendZone <strile93@gmail.com>
2016-10-18 00:36:46 +0200

    Implemented login check.

--- app/src/main/java/com/example/gencode/execomtodolist/activity/MainActivity.java
+++ app/src/main/java/com/example/gencode/execomtodolist/activity/MainActivity.java
@@ -3,11 +3,17 @@ package com.example.gencode.execomtodolist.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.example.gencode.execomtodolist.DAO.TaskDao;
 import com.example.gencode.execomtodolist.DAO.UserDao;
 import com.example.gencode.execomtodolist.R;
 import com.example.gencode.execomtodolist.database.DatabaseManager;
 import com.example.gencode.execomtodolist.model.User;
+
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +30,37 @@ public class MainActivity extends AppCompatActivity {
         this.initDB();
         this.insertUsers();
 
+        Login();
+
+    }
+
+    private void Login() {
+        Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText username = (EditText) findViewById(R.id.username);
+                String usernameText = username.getText().toString();
+
+                EditText password = (EditText) findViewById(R.id.password);
+                String passwordText = password.getText().toString();
+
+                checkUsernameAndPassword(usernameText, passwordText);
+            }
+        });
+    }
+
+    private void checkUsernameAndPassword(String username, String password){
+        List<User> users = (List<User>) userDao.findAll();
+
+        for(User user: users){
+            if(user.getUsername().equals(username) && user.getPassword().equals(password)){
+                //predji na sledecu aktivnost
+                Log.i("LOGIN", "Login is sucsessful.");
+                break;
+            }
+        }
+        Log.i("LOGIN", "Login has failed.");
     }
 
 
